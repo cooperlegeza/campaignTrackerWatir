3f8122aff5a79ca2aabb26a52dc0c09992fa5e88
Cooper Legeza <cooperlegeza@gmail.com>
2016-12-14 12:09:12 -0500

    Adding step definitions

--- features/step_definitions/calender_construction_steps.rb
+++ features/step_definitions/calender_construction_steps.rb
@@ -31,28 +31,78 @@ When(/^I remove an event from the timeline$/) do
 end
 
 Then(/^I should not see the event in the calender$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending #TODO: Write page object
+  on_page 
 end
 
 And(/^the event should be removed from the factsheets$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending #TODO: Write page object
+  on_page Calender do |page|
+    page.factsheets
+  end
+
+  on_page FactsheetsHome do |page|
+    expect(page.factsheet_titles).to not_include @title
+  end
 end
+
 When(/^I mark an event as hidden$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending #TODO: Write page object
+  on_page Calender do |page|
+    page.first_event
+    page.make_hidden
+  end
 end
 
 Then(/^players are not able to see that event$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending #TODO: Write page object
+  on_page Calender do |page|
+    page.logout
+    page.login
+  end
+
+  on_page LoginPage do |page|
+    page.username = PLAYER_USERNAME
+    page.password = PLAYER_PASSWORD
+  end
+
+  on_page HomePage do |page|
+    page.calenders
+  end
+
+  on_page CalendersHome do |page|
+    page.calender
+  end
+
+  on_page Calender do |page|
+    expect(page.event_titles).to not_include THE_FINAL_BATTLE
+  end
 end
 
 When(/^I click on an event$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending #TODO: Write page object
+  on_page Calender do |page|
+    page.first_event
+  end
 end
 
 Then(/^I should be able to add information to the event$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending #TODO: Write page object
+  on_page Calender do |page|
+    page.add_event_title = THE_FINAL_BATTLE
+    page.add_event_chars(THE_MAJOR_PLAYERS)
+    page.add__event_description = THE_FINAL_BATTLE_DESC
+
+    expect(page.event_title).to eq THE_FINAL_BATTLE
+    expect(page.event_chars).to match_array THE_MAJOR_PLAYERS
+    expect(page.event_description).to eq THE_FINAL_BATTLE_DESC
+  end
 end
 
 Then(/^I should be able to link the event to a location in a map$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending #TODO: Write page object
+  on_page Calender do |page|
+    page.add_location = THE_TOWER
+    expect(page.first_location).to eq THE_TOWER
+  end
 end
---
+++ features/support/constants.rb
@@ -0,0 +1,4 @@
+THE_TOWER = "The Tower of Bel-Sharok"
+THE_FINAL_BATTLE = "The Final Battle of the Second Demon War"
+THE_MAJOR_PLAYERS = ["Bel-Sharok", "Cyral the Light", "Shoon, Eater of the Dead", "Lydia of Many Arrows", "They of Hidden Face"]
+THE_FINAL_BATTLE_DESC = "The final battle of the Second Demon War, a combined force of 60,000 humans, elves, dwarves, and shirgal fought to destroy Bal-Sharok, the Doomer"
